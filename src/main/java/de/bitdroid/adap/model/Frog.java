728a3d6a9ea42fae52d85476ff622e9aff8bf4b3
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-11-23 16:14:18 +0100

    model: added habitats field to Frog

--- src/main/java/de/bitdroid/adap/model/Frog.java
+++ src/main/java/de/bitdroid/adap/model/Frog.java
@@ -1,27 +1,34 @@
 package de.bitdroid.adap.model;
 
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
 import org.wahlzeit.utils.Assert;
 
 import java.util.EnumSet;
+import java.util.Set;
 
 public abstract class Frog {
 
 	private final String commonName, scientificName;
-	private final EnumSet<Animals> diet;
+	private final ImmutableSet<Animals> diet;
 	private final NumberRange<Double> sizeRange;
+	private final ImmutableSet<Area> habitats;
 
 	public Frog(
 			String commonName,
 			String scientificName,
 			EnumSet<Animals> diet,
-			NumberRange<Double> sizeRange) {
+			NumberRange<Double> sizeRange,
+			Set<Area> habitats) {
 
-		Assert.assertNotNull(commonName, scientificName, diet, sizeRange);
+		Assert.assertNotNull(commonName, scientificName, diet, sizeRange, habitats);
 		this.commonName = commonName;
 		this.scientificName = scientificName;
-		this.diet = diet;
+		this.diet = Sets.immutableEnumSet(diet);
 		this.sizeRange = sizeRange;
+		this.habitats = ImmutableSet.copyOf(habitats);
 	}
 
 
@@ -35,13 +42,18 @@ public abstract class Frog {
 	}
 
 
-	public EnumSet<Animals> getDiet() {
+	public ImmutableSet<Animals> getDiet() {
 		return diet;
 	}
 
 
 	public NumberRange<Double> getSizeRange() {
 		return sizeRange;
+	}
+
+
+	public ImmutableSet<Area> getHabitats() {
+		return habitats;
 	}
 
 }
