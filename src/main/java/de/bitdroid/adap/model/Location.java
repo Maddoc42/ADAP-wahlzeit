46db5a012fa8340bfeb12e39953f7f2fe5b85ecc
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-12-30 13:42:37 +0100

    model: added some doc to Location

--- src/main/java/de/bitdroid/adap/model/Location.java
+++ src/main/java/de/bitdroid/adap/model/Location.java
@@ -9,7 +9,19 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "class")
 public interface Location {
 
+	/**
+	 * Computes the distance between this location and another.
+	 * @param location Must be of the same type as this location! TODO not very pretty ...
+	 * @return the distance between this location and the received location.
+	 */
 	public double computeDistanceTo(Location location);
+
+	/**
+	 * Used by the visitor pattern of {@link de.bitdroid.adap.model.LocationVisitor}
+	 * @param visitor The visitor to execute.
+	 * @param param The parameter to pass to the visitor.
+	 * @return The result of the visitor.
+	 */
 	public <P,R> R accept(LocationVisitor<P,R> visitor, P param);
 
 }
